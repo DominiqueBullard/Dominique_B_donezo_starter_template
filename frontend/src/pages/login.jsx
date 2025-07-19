cad297f27121fa722561ef9839f72039aead5278
Your Name <you@example.com>
2025-07-19 14:56:54 -0400

    frontend logun and signup

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,4 +1,29 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import supabase from "../client";
+
+const [alert, showAlert] = useState({message: "",show: false});
+const { register, handleSubmit } = useForm({
+  defaultValues: {
+    email: "",
+    password: ""
+  }
+});
+const navigate = useNavigate();
+
+const loginUser = async (values) => {
+	 const { error } = await supabase.auth.signInWithPassword({ email, password });
+
+	if (error) {
+	  showAlert({
+	    show: true, 
+	    message: error.message 
+	  });
+	} else {
+	  navigate("/todos.jsx");
+	}
+};
 
 export default function Login() {
 
@@ -7,7 +32,54 @@ export default function Login() {
     <div className="bg-gray-100 flex items-center justify-center min-h-screen">
       <div className="max-w-md w-full bg-white p-6 rounded-lg shadow-md">
         <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
-        {/* Add LoginForm Here.... */}
+        return (
+    <>
+      {
+        alert.show &&
+          <div className="alert alert-error">
+            <div className="inline-flex justify-stretch items-center">
+              {alert.message}
+              <button onClick={() => showAlert({ message: "", show: false })} className="btn btn-ghost btn-circle">
+                X
+              </button>
+            </div>
+          </div>
+      }
+    </>
+ )
+        {function LoginForm() {
+    return (
+      <form className="space-y-4" onSubmit={handleSubmit(loginUser)}>
+       <div>
+  <label 
+	  htmlFor="email" 
+	  className="block text-sm font-medium text-gray-700">
+		  Email
+	  </label>
+  <input 
+	  id="email" 
+	  type="email" 
+	  className="input input-bordered w-full" 
+	  { ...register("email") } 
+	 />
+</div>
+<div>
+  <label 
+	  htmlFor="password" 
+	  className="block text-sm font-medium text-gray-700">
+	  Password
+	 </label>
+  <input 
+	  id="password" 
+	  type="password" 
+	  className="input input-bordered w-full" 
+	  { ...register("password") } 
+	 />
+</div>
+        <button type="submit" className="btn btn-primary w-full">Login</button>
+      </form>
+    )
+}}
         <p className="mt-4 text-center text-sm">Don't have an account? <Link to="/signup" className="text-blue-500 hover:underline">Sign up</Link></p>
       </div>
     </div>
--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -1,10 +1,78 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import supabase from "../client";
+
+  const [alert, showAlert] = useState({
+    message: "",
+    show: false
+  });
+
+  const navigate = useNavigate();
+
+    const { register, handleSubmit } = useForm({
+    defaultValues: {
+      username: "",
+      email: "",
+      password: ""
+    }
+  });
+
+    const signupUser = async (values) => {
+    const { error } = await supabase.auth.signUp({
+      email: values.email,
+      password: values.password,
+      data: {
+        username: values.username
+      }
+    });
+
+    if(error){
+      showAlert({
+        show: true,
+        message: error.message
+      })
+    } else {
+      navigate("/todos");      
+    }
+  }
+
 export default function Signup(){
   return (
    <div className="bg-gray-100 flex items-center justify-center min-h-screen">
       <div className="max-w-md w-full bg-white p-6 rounded-lg shadow-md">
         <h2 className="text-2xl font-bold mb-6 text-center">Signup</h2>
-          {/* Render SignUpAlert here... */}
-          {/* Render SignupForm here... */}
+          {return (
+  <>
+    {alert.show &&
+      <div className="alert alert-error">
+        <div className="inline-flex justify-stretch items-center">
+          {alert.message}
+          <button onClick={() => showAlert({ message: "", show: false })} className="btn btn-ghost btn-circle">
+            X
+          </button>
+        </div>
+      </div>
+    }
+  </>
+)}
+{return (
+  <form className="space-y-4" onSubmit={handleSubmit(signupUser)}>
+    <div>
+      <label htmlFor="username" className="block text-sm font-medium text-gray-700">Username</label>
+      <input id="username" type="username" className="input input-bordered w-full"  />
+    </div>
+    <div>
+      <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
+      <input id="email" type="email" className="input input-bordered w-full" />
+    </div>
+    <div>
+      <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+      <input id="password" type="password" className="input input-bordered w-full" />
+    </div>
+    <button type="submit" className="btn btn-primary w-full">Signup</button>
+  </form>
+)}
         <p className="mt-4 text-center text-sm">Have an account? <Link to="/login" className="text-blue-500 hover:underline">Login</Link></p>
       </div>
     </div> 
